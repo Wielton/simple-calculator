7adffe56675578b894c93b1875bc6c8e3fb3cdbc
Wielton <grantwielgosz@gmail.com>
2022-12-04 00:04:52 -0700

    Created calculator UI and some functions

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,58 +1,126 @@
 <template>
-  <div class="hello">
-    <h1>{{ msg }}</h1>
-    <p>
-      For a guide and recipes on how to configure / customize this project,<br>
-      check out the
-      <a href="https://cli.vuejs.org" target="_blank" rel="noopener">vue-cli documentation</a>.
-    </p>
-    <h3>Installed CLI Plugins</h3>
-    <ul>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-babel" target="_blank" rel="noopener">babel</a></li>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-eslint" target="_blank" rel="noopener">eslint</a></li>
-    </ul>
-    <h3>Essential Links</h3>
-    <ul>
-      <li><a href="https://vuejs.org" target="_blank" rel="noopener">Core Docs</a></li>
-      <li><a href="https://forum.vuejs.org" target="_blank" rel="noopener">Forum</a></li>
-      <li><a href="https://chat.vuejs.org" target="_blank" rel="noopener">Community Chat</a></li>
-      <li><a href="https://twitter.com/vuejs" target="_blank" rel="noopener">Twitter</a></li>
-      <li><a href="https://news.vuejs.org" target="_blank" rel="noopener">News</a></li>
-    </ul>
-    <h3>Ecosystem</h3>
-    <ul>
-      <li><a href="https://router.vuejs.org" target="_blank" rel="noopener">vue-router</a></li>
-      <li><a href="https://vuex.vuejs.org" target="_blank" rel="noopener">vuex</a></li>
-      <li><a href="https://github.com/vuejs/vue-devtools#vue-devtools" target="_blank" rel="noopener">vue-devtools</a></li>
-      <li><a href="https://vue-loader.vuejs.org" target="_blank" rel="noopener">vue-loader</a></li>
-      <li><a href="https://github.com/vuejs/awesome-vue" target="_blank" rel="noopener">awesome-vue</a></li>
-    </ul>
-  </div>
+  <v-container fluid>
+    <v-row class="pa-6">
+      <v-col cols="12">
+        <v-card outlined height="50" class="pa-6 d-flex align-center">{{ displayValue }}</v-card>
+      </v-col>
+    </v-row>
+    <v-row class="ma-auto">
+      <v-col cols="3" class="pa-6 d-flex justify-center">
+        <v-btn outlined class="fb-btn" v-model="one" @click="updateDisplay(one)">1</v-btn>
+      </v-col>
+      <v-col cols="3" class="pa-6 d-flex justify-center">
+        <v-btn outlined class="fb-btn" v-model="two" @click="updateDisplay(two)">2</v-btn>
+      </v-col>
+      <v-col cols="3" class="pa-6 d-flex justify-center">
+        <v-btn outlined class="fb-btn" v-model="three" @click="updateDisplay(three)">3</v-btn>
+      </v-col>
+      <v-col cols="3" class="pa-6 d-flex justify-center">
+        <v-btn outlined class="fb-btn" @click="chooseOperator('add')">+</v-btn>
+      </v-col>
+    </v-row>
+    <v-row class="ma-auto">
+      <v-col cols="3" class="pa-6 d-flex justify-center">
+        <v-btn outlined class="fb-btn" v-model="four" @click="updateDisplay(four)">4</v-btn>
+      </v-col>
+      <v-col cols="3" class="pa-6 d-flex justify-center">
+        <v-btn outlined class="fb-btn" v-model="five" @click="updateDisplay(five)">5</v-btn>
+      </v-col>
+      <v-col cols="3" class="pa-6 d-flex justify-center">
+        <v-btn outlined class="fb-btn" v-model="six" @click="updateDisplay(six)">6</v-btn>
+      </v-col>
+      <v-col cols="3" class="pa-6 d-flex justify-center">
+        <v-btn outlined class="fb-btn">-</v-btn>
+    </v-col>
+    </v-row>
+    <v-row class="ma-auto">
+      <v-col cols="3" class="pa-6 d-flex justify-center">
+        <v-btn outlined class="fb-btn" v-model="seven" @click="updateDisplay(seven)">7</v-btn>
+      </v-col>
+      <v-col cols="3" class="pa-6 d-flex justify-center">
+        <v-btn outlined class="fb-btn" v-model="eight" @click="updateDisplay(eight)">8</v-btn>
+      </v-col>
+      <v-col cols="3" class="pa-6 d-flex justify-center">
+        <v-btn outlined class="fb-btn" v-model="nine" @click="updateDisplay(nine)">9</v-btn>
+      </v-col>
+      <v-col cols="3" class="pa-6 d-flex justify-center">
+        <v-btn outlined class="fb-btn">x</v-btn>
+      </v-col>
+    </v-row>
+    <v-row class="ma-auto">
+      <v-col cols="3" class="pa-6 d-flex justify-center" >
+        <v-btn outlined class="fb-btn" @click="clear">C</v-btn>
+      </v-col>
+      <v-col cols="3" class="pa-6 d-flex justify-center">
+          <v-btn outlined class="fb-btn" v-model="zero" @click="updateDisplay(zero)">0</v-btn>
+      </v-col>
+      <v-col cols="3" class="pa-6 d-flex justify-center">
+          <v-btn outlined class="fb-btn">=</v-btn>
+      </v-col>
+      <v-col cols="3" class="pa-6 d-flex justify-center">
+          <v-btn outlined class="fb-btn">/</v-btn>
+      </v-col>
+    </v-row>
+  </v-container>
 </template>
 
 <script>
-export default {
-  name: 'HelloWorld',
-  props: {
-    msg: String
-  }
-}
-</script>
+  export default {
+    name: 'HelloWorld',
 
-<!-- Add "scoped" attribute to limit CSS to this component only -->
-<style scoped lang="scss">
-h3 {
-  margin: 40px 0 0;
-}
-ul {
-  list-style-type: none;
-  padding: 0;
-}
-li {
-  display: inline-block;
-  margin: 0 10px;
-}
-a {
-  color: #42b983;
+    data: () => ({
+      displayValue: '',
+      firstOperand: null,
+      waitingForSecondOperand: false,
+      operator: null,
+      one: 1,
+      two: 2,
+      three: 3,
+      four: 4,
+      five: 5,
+      six: 6,
+      seven: 7,
+      eight: 8,
+      nine: 9,
+      zero: 0,
+    }),
+    methods: {
+      logEvent(e){
+        console.log(e)
+      },
+      // Have an empty array to hold the equation so that the data types are retained
+      // Have a function to add the given value from the corresponding button to the equation calculator: {
+      //                                                                                                    firstOperand: null,
+      //                                                                                                    waitingForSecondOperand: false,
+      //                                                                                                    operator: null
+      //                                                                                                  }
+      // I should make an instance of a calculator to track the values instead of tracking individually like it is
+      clear(){this.displayValue = ''},
+      appendNum(num){ this.firstOperand= num },
+      chooseOperator(operation){ 
+        this.firstOperand = this.displayValue 
+        if(operation == 'add'){
+          this.operator = '+'
+        }else if(operation == 'subtract'){
+          this.operator = '-'
+        }else if(operation == 'multiply'){
+          this.operator = '*'
+        }else{
+          this.operator = '/'
+        }
+        this.displayValue = this.firstOperand + this.operator
+        console.log("Your equation so far is: " + this.firstOperand + ' ' + this.operator)
+      },
+      compute(){},
+      updateDisplay(item){ 
+        this.displayValue += item
+        }
+    }
+  }
+</script>
+<style lang="scss" scoped>
+.fb-btn.v-btn--outlined {
+  border: thin solid aqua;
+  color: aqua;
 }
 </style>
