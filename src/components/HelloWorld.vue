ad2bc2a917dfa264c824ba3ad8c8f0d42f3fe74c
Wielton <grantwielgosz@gmail.com>
2022-12-06 01:36:38 -0700

    Working on addition function first, trying to capture proper data types in proper sequence for a mathematical equation

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -7,10 +7,10 @@
     </v-row>
     <v-row class="ma-auto">
       <v-col cols="3" class="pa-6 d-flex justify-center" v-for="numBtn in calculatorNumbers" :key="numBtn.id">
-        <v-btn :numBtn="numBtn" outlined class="num-btn" @click="updateDisplay(numBtn.btnValue)">{{numBtn.numName}}</v-btn>
+        <v-btn :numBtn="numBtn" outlined class="num-btn" @click="updateDisplay(numBtn.btnValue); addNumber(numBtn.btnValue)">{{numBtn.numName}}</v-btn>
       </v-col>
       <v-col cols="3" class="pa-6 d-flex justify-center" v-for="funcBtn in calculatorFunctions" :key="funcBtn.id">
-        <v-btn :funcBtn="funcBtn" outlined class="func-btn" :color="funcBtn.btnColor ? funcBtn.btnColor : inherit" @click="chooseFunction(funcBtn.func)">{{funcBtn.funcName}}</v-btn>
+        <v-btn :funcBtn="funcBtn" outlined class="func-btn" :color="funcBtn.btnColor ? funcBtn.btnColor : 'aqua'" @click="chooseFunction(funcBtn.func)">{{funcBtn.funcName}}</v-btn>
       </v-col>
     </v-row>
   </v-container>
@@ -24,8 +24,9 @@
       displayValue: '',
       firstOperand: null,
       secondOperand: null,
-      isWaitingForSecondOperand: false,
+      isWaitingForSecondOperand: true,
       operator: null,
+      calculatedResult: null,
       calculatorNumbers: [
         { id: 1, numName: '1', btnValue: 1 },
         { id: 2, numName: '2', btnValue: 2 },
@@ -60,19 +61,27 @@
       //                                                                                                  }
       // I should make an instance of a calculator to track the values instead of tracking individually like it is
       clearDisplay(){this.displayValue = ''},
+      
       adding(){console.log('Adding...')},
+      
       subtract(){console.log('Subtracting...')},
+      
       multiply(){console.log('Multiplying...')},
+      
       divide(){console.log('Dividing...')},
-      solve(){console.log('Solving...')},
+      
+      // solve(num1,num2,func){
+      //   this.calculatedResult = num1 + func + num2
+      //   console.log('Solving...')
+      // },
       
       chooseFunction(str){
-          this.firstOperand = this.displayValue
-          this.isWaitingForSecondOperand = true
+          this.firstOperand = +this.displayValue
+          
           if(str == 'add' && this.operator === null){
             this.adding()
             this.operator = '+'
-            this.displayValue = this.operator
+            this.updateDisplay(this.operator)
             console.log("Your equation so far is: " + this.firstOperand + this.operator)
           }else if(str == 'subtract' && this.operator === null){
             this.subtract()
@@ -90,19 +99,34 @@
             this.displayValue = this.firstOperand + this.operator
             console.log("Your equation so far is: " + this.firstOperand + ' ' + this.operator)
           }else if(str == 'clearDisplay'){
-            // this.operator = null
-            // this.firstOperand = null
+            this.operator = null
+            this.firstOperand = null
             console.log(this.operator, this.firstOperand)
             this.displayValue = ''
           }else if(str == 'decimal'){
             this.displayValue += '.'
           }
           else{
-            this.solve()
-            this.operator = '='
+            if(this.firstOperand !== null && this.lastOperand !== null){
+              if(this.operator == '+'){
+                console.log(this.firstOperand)
+                // this.calculatedResult = +this.firstOperand + this.lastOperand
+                // console.log("Solving...")
+                // console.log(this.calculatedResult)
+              }
+            }
+            
           }
         },
-      
+      addNumber(item){
+        if(this.operator !== null){
+          this.secondOperand = item
+          console.log("The second operand: " + this.secondOperand)
+        }else{
+          this.firstOperand = +this.displayValue
+          console.log("The first operand: " + this.firstOperand)
+        }
+      },
       updateDisplay(item){ 
         this.displayValue += item
         }
